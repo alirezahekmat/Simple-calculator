e60c9535bd5813d6290d0e25a04c491250c9ecf3
AliReza <alirezahekmat724@gmail.com>
2021-01-14 18:28:57 +0330

    'CorrectFile'

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 a = int(input())
+c = input()
 b = int(input())
-i=0
-j=0
-while i<b:
-    while j<a:
-        if i==j or i+j==a-1:
-            print(end="*")
-        else:
-            print(end=" ")
-        j+=1
-    print(end="\n")
-    i+=1
-    j=0
+if c=="+":
+    print(a + b)
+if c=="-":
+    print(a - b)
+if c=="*" or b=="×":
+    print(a * b)
+if c=="/":
+    print(a / b)
