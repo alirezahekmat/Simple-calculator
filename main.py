78e418d49b9aae1e140adb53be2ff742129e729f
AliReza <alirezahekmat724@gmail.com>
2021-01-23 17:04:18 +0330

    'OOP_version'

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 a = int(input())
 c = input()
 b = int(input())
+class Calc:
+    def sum(self,first_number,secend_number):
+        return  first_number + secend_number
+    def sub(self,first_number,secend_number):
+        return first_number - secend_number
+    def multi(self,first_number,secend_number):
+        return first_number * secend_number
+    def div(self,first_number,secend_number):
+        return first_number / secend_number
+calc=Calc();
+
 if c=="+":
-    print(a + b)
+    print(calc.sum(a,b))
 if c=="-":
-    print(a - b)
-if c=="*" or b=="×":
-    print(a * b)
+    print(calc.sub(a,b))
+if c=="*" or c=="×":
+    print(calc.multi(a,b))
 if c=="/":
-    print(a / b)
+    print(calc.div(a,b))
